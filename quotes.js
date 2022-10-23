8f39c432e08e8782c490beaa302716800a32f096
codingmemories <you@example.com>
2022-10-23 16:44:52 +0300

    add content to quotes.js file

--- quotes.js
+++ quotes.js
@@ -16,7 +16,55 @@ const localQuotes = [
     author: "Robson",
   },
   {
-    text: "No kurwa, znowu ten inteligent, mnie przeszkadza.",
+    text: "Oddzwonię za chwilę! 🌀🌀🌀5 lat później, a Robson nie oddzwonił.",
+    author: "Robson",
+  },
+  {
+    text: "Rob: Ten poniedziałek w ogóle pojebany! Janusz: Ale dzisiaj czwartek.",
+    author: "Robson",
+  },
+  {
+    text: "Nigdy nic nie robie w ostatnio chwilę.",
+    author: "Robson",
+  },
+  {
+    text: "Tylko jedno w głowie mam, koksu 5 gram.",
+    author: "Robson",
+  },
+  {
+    text: "Masz jakiś drobne? Daj mi 5 złotych i ja spierdalam.",
+    author: "Robson",
+  },
+  {
+    text: "Całe życie zapierdalam i nigdy nie mam pieniędzy.",
+    author: "Robson",
+  },
+  {
+    text: "Jutro jedziemy do prawnika, on wszytko załatwia.",
+    author: "Robson",
+  },
+  {
+    text: "W urzędach są głupki. Muszę to zmienić.",
+    author: "Robson",
+  },
+  {
+    text: "Następnym tygodniu jedziemy robić pożarowkę do Londynu.",
+    author: "Robson",
+  },
+  {
+    text: "Dzisiaj znowu będę spać w busie.",
+    author: "Robson",
+  },
+  {
+    text: "W poniedziałek będzie konkretny pieniądz.",
+    author: "Robson",
+  },
+  {
+    text: "Janusz: Rob, poco ci tyle pudełek? Jesteś jak stary dziad.",
+    author: "Robson",
+  },
+  {
+    text: "Marek: Robert ciebie szukał Janusz. Prosi cię oddzwonić. Robert: Nie mam czasu na te głupoty.",
     author: "Robson",
   },
 ];
--- script.js
+++ script.js
@@ -30,6 +30,19 @@ function newQuote() {
   complete();
 }
 
+function clicked() {
+  if (img == 1) {
+    document.getElementById("img").style.display = "inline";
+    return (img = 0);
+  } else {
+    document.getElementById("img").style.display = "none";
+    return (img = 1);
+  }
+}
+
+const audio = new Audio();
+audio.src = "./assets/audio/gta.mp3";
+
 function tweetQuote() {
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
   window.open(twitterUrl, "_blank");
